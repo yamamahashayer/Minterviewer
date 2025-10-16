53768ad57d1a53f745e36ed8c789954f6b51d8e6
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-10-16 10:07:04 +0300

    Home Page and its Compontnes Styling

--- minterviewer/app/(root)/layout.tsx
+++ minterviewer/app/(root)/layout.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-const Rootlayout = ({children }: {children: React.ReactNode}) => {
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+
   return (
-    <div>{children}</div>
-  )
-}
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">Minterviewer</h2>
+        </Link>
+      </nav>
 
-export default Rootlayout
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
--- minterviewer/app/(root)/page.tsx
+++ minterviewer/app/(root)/page.tsx
@@ -1,8 +1,47 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/dist/client/link'
 import React from 'react'
-
+import Image from 'next/image'
+import { dummyInterviews } from '@/constants'
+import InterviewCard from '@/components/InterviewCard'
 const page = () => {
   return (
-    <div> Home page</div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gab-6 max-w-lg ">
+          <h2>Get Interview Ready With Ai-Powered Practice & FeedBack </h2>
+          <p className="text-lg">
+            Practice on real interview questions, get instant feedback, and improve your skills with our AI-powered platform.
+          </p>
+          <Button asChild className="btn-primary">
+            <Link href="/interview">  Start An Interview
+            </Link>
+          </Button>
+        </div>
+        <Image src="/robot.png" alt="Robo-Dude" width={400} height={400} className="max-sm:hidden" />
+      </section>
+      <section className="flex flex-col gap-6 mt-8 ">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+        {/* <p>You haven&apos;t taken any interviews yet.</p> */}
+      </section>
+      <section className="flex flex-col gap-6 mt-8 ">
+        <h2>Take an Interview </h2>
+        <div className="interviews-section">
+          <div className="interviews-section">
+            {dummyInterviews.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))}
+          </div>
+
+
+        </div>
+      </section>
+    </>
   )
 }
 
