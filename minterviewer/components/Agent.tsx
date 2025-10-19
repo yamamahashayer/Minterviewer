f097a98945035d10041e70ab20d1867ed9199635
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-10-19 11:16:34 +0300

    IMPLEMETN INTERVIEW GENERATION PAGE UI

---
+++ minterviewer/components/Agent.tsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+enum CallStatus {
+    INACTIVE = "INACTIVE",
+    CONNECTING = "CONNECTING",
+    ACTIVE = "ACTIVE",
+    FINISHED = "FINISHED",
+}
+
+interface SavedMessage {
+    role: "user" | "system" | "assistant";
+    content: string;
+}
+
+const Agent = ({ userName, userId, type }: AgentProps) => {
+    const isSpeaking = true; // Replace with actual speaking status
+    const callStatus = CallStatus.FINISHED; // Replace with actual call status
+    const messages = [
+        'whats your name?',
+        'my name is JOHN DOE , NICE TO MEET YOU!'// Replace with actual messages,
+
+    ];
+    const lastMessage = messages[messages.length - 1];
+    return (
+        <>
+            <div className="call-view">
+                {/* AI Interviewer Card */}
+                <div className="card-interviewer">
+                    <div className="avatar">
+                        <Image
+                            src="/ai-avatar.png"
+                            alt="vapi"
+                            width={65}
+                            height={54}
+                            className="object-cover"
+                        />
+                        {isSpeaking && <span className="animate-speak" />}
+                    </div>
+                    <h3>AI Interviewer</h3>
+                </div>
+
+                {/* User Profile Card */}
+                <div className="card-border">
+                    <div className="card-content">
+                        <Image
+                            src="/user-avatar.png"
+                            alt="profile-image"
+                            width={540}
+                            height={540}
+                            className="rounded-full object-cover size-[120px]"
+                        />
+                        <h3>{userName}</h3>
+                    </div>
+                </div>
+            </div>
+
+            {messages.length > 0 && ( // >0 WE HAVE MESSAGES 
+                <div className="transcript-border">
+                    <div className="transcript">
+                        <p
+                            key={lastMessage}
+                            className={cn(
+                                "transition-opacity duration-500 opacity-0",
+                                "animate-fadeIn opacity-100"
+                            )}
+                        >
+                            {lastMessage}
+                        </p>
+                    </div>
+                </div>
+            )}
+
+            <div className="w-full flex justify-center">
+                {callStatus !== "ACTIVE" ? (
+                    <button className="relative btn-call" >
+                        <span
+                            className={cn(
+                                "absolute animate-ping rounded-full opacity-75",
+                                callStatus !== "CONNECTING" && "hidden"
+                            )}
+                        />
+
+                        <span className="relative">
+                            {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                                ? "Call"
+                                : ". . ."}
+                        </span>
+                    </button>
+                ) : (
+                    <button className="btn-disconnect" >
+                        End
+                    </button>
+                )}
+            </div>
+        </>
+    )
+}
+
+export default Agent
