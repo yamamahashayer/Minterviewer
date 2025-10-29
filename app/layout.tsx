f9258b352a82b49cb55f9a0b2fd504de25c2a2e9
yamamah ashayer <yamamahashayer@gmail.com>
2025-10-29 16:00:05 +0300

    edit layout +packege

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,32 @@
-// app/layout.tsx
 import type { Metadata } from "next";
+import { Geist } from "next/font/google";
 import "./globals.css";
+//import { ThemeProvider } from "@/Context/ThemeContext";
+//import { Toaster } from "react-hot-toast";
 
-
+const geist = Geist({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Minterviewer",
-  description: "AI Interview & Mentorship Platform",
+  description: "Practice mock interviews with experts and get real feedback",
+  keywords: ["interviews", "mock interviews", "practice", "career", "mentoring"],
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={geist.className}>
+        {/* <ThemeProvider>
+          // {children}
+          <Toaster />
+        </ThemeProvider> */}
         {children}
       </body>
     </html>
