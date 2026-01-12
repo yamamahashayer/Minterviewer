ce17b87a5e4b8887611046dc3909ed0dc86e6851
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2026-01-12 19:04:12 +0200

    Layout for bublic pages

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -11,11 +11,9 @@ export default function PublicLayout({
 }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
       <main className="flex-grow">
         {children}
       </main>
-      <Footer />
     </div>
   );
 }
